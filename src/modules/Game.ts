e7e064a487d51dc2e2e2f5a57a526babe0c04517
AJ Lagutan <ajlagutan@outlook.com>
2026-04-18 02:15:33 +0800

    Add lil-gui component

--- src/modules/Game.ts
+++ src/modules/Game.ts
@@ -1,7 +1,9 @@
+import * as lil from "lil-gui";
 import { Graphics } from "./Graphics";
 import { Keyboard } from "./Keyboard";
 import { Mouse } from "./Mouse";
 import { debug } from "./Utils";
+type GameOptions = { fps: boolean };
 /**
  * A class that handles the main game loop.
  *
@@ -14,12 +16,14 @@ import { debug } from "./Utils";
  * @class
  */
 export abstract class Game {
+  private static _accumulator: number = 0;
   private static _fps: number = 60;
+  private static _gui: lil.GUI;
+  private static _lastUpdateTime: number = 0;
+  private static _options: GameOptions = { fps: false };
   private static _scene: any;
   private static _step: number = 1 / this._fps;
   private static _stopped: boolean;
-  private static _lastUpdateTime: number = 0;
-  private static _accumulator: number = 0;
   /**
    * This is a static class, do not invoke the class constructor.
    *
@@ -45,6 +49,7 @@ export abstract class Game {
     try {
       debug(this, "starting...");
       this.init();
+      this.initGui();
       this.requestUpdate();
       debug(this, "started.");
     } catch (error) {
@@ -67,6 +72,32 @@ export abstract class Game {
     Mouse.init();
     // Graphics module
     Graphics.init();
+  }
+  private static initGui(): void {
+    this._gui = new lil.GUI({ title: "control panel" });
+    this._gui.add(this._options, "fps").onFinishChange(Graphics.toggleFps.bind(Graphics)).name("show fps");
+    this._gui.onFinishChange(this.saveOptions);
+    this.loadOptions(this._gui);
+  }
+  /**
+   * Loads the options from the browser's {@link localStorage} module.
+   *
+   *
+   *
+   * @private
+   * @static
+   * @method
+   * @param {lil.GUI} gui A root {@link lil.GUI} instance.
+   * @returns {void}
+   */
+  private static loadOptions(gui: lil.GUI): void {
+    try {
+      const s = localStorage.getItem("/:root") ?? "{}";
+      const o = JSON.parse(s);
+      gui.load(o, true);
+    } catch (error) {
+      console.error(error);
+    }
   }
   /**
    * The main game loop logic.
@@ -125,6 +156,30 @@ export abstract class Game {
   private static requestUpdate(): void {
     if (!this._stopped) {
       requestAnimationFrame(this.loop.bind(this));
+    }
+  }
+  /**
+   * Saves the options to the browser's {@link localStorage} module.
+   *
+   *
+   *
+   * @private
+   * @static
+   * @method
+   * @param event The change event.
+   * @returns {void}
+   */
+  private static saveOptions(event: any): void {
+    try {
+      if (event["controller"] instanceof lil.Controller) {
+        const controller: lil.Controller = event["controller"];
+        const gui: lil.GUI = controller.parent;
+        const o = gui.save(true);
+        const s = JSON.stringify(o);
+        localStorage.setItem("/:root", s);
+      }
+    } catch (error) {
+      console.error(error);
     }
   }
   /**
