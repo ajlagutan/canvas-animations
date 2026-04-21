762230ce71163d89abe88696696a0ef1243ff640
AJ Lagutan <ajlagutan@outlook.com>
2026-04-22 00:15:01 +0800

    Update main entry point

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { Game } from "./modules/Game";
 import "./style.css";
 
-(function () {
-  Game.run();
-})();
+import { Game } from "./_modules";
+import * as Scenes from "./_scenes";
+
+Game.run((manager) => {
+  manager.register(Scenes.Scene_Test, "57fe0d3ee1b34399b464053e459eb18d");
+  manager.register(Scenes.Scene_Particle, "841e5a91ffaf4432b3db034b520a2d52");
+});
