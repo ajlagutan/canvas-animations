7aa232c40a292815fd6aeb83e6491d5dff121653
AJ Lagutan <ajlagutan@outlook.com>
2026-04-26 23:52:32 +0800

    Add approach and decay functions

--- src/lib/utils/src/Mathematics.ts
+++ src/lib/utils/src/Mathematics.ts
@@ -1,8 +1,39 @@
 /**
+ * Smooth approach towards the {@link targer} value.
+ *
+ *
+ *
+ * @public
+ * @function
+ * @param value The current value.
+ * @param target The target value.
+ * @param duration The duration (in frames) of the approach.
+ * @returns {number}
+ */
+export function approach(value: number, target: number, duration: number): number {
+  return value + (target - value) / duration;
+}
+/**
+ * Smooth decay towards zero.
+ *
+ *
+ *
+ * @public
+ * @function
+ * @param value The current value to decay.
+ * @param duration The duration (in frames) of the decay.
+ * @returns {number}
+ */
+export function decay(value: number, duration: number): number {
+  return value - value / duration;
+}
+/**
  * Returns the mathematical modulo (always non-negative).
  *
  *
  *
+ * @public
+ * @function
  * @param n The dividend.
  * @param m The divisor (must be non-zero).
  * @returns {number} The mathematical modulo (positive) result.
