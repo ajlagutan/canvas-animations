0ce667cc7310bf1a827c8b22f0c1c702a60f9699
AJ Lagutan <ajlagutan@outlook.com>
2026-04-26 22:48:18 +0800

    Delete previous code structure

--- src/_modules.ts
+++
@@ -1,8 +0,0 @@
-export * as "lil" from "lil-gui";
-
-export * from "./modules/Game";
-export * from "./modules/Graphics";
-export * from "./modules/Keyboard";
-export * from "./modules/Mouse";
-export * from "./modules/Scene";
-export * from "./modules/Utils";
--- src/_scenes.ts
+++
@@ -1,3 +0,0 @@
-export * from "./scenes/Scene_Particle";
-export * from "./scenes/Scene_Particle2";
-export * from "./scenes/Scene_Test";
