679c2479d79dff7922328a885efe0ccbfc6cf092
AJ Lagutan <ajlagutan@outlook.com>
2026-04-26 22:50:19 +0800

    Update previous configurations

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import { NodePackageImporter } from "sass-embedded";
 
 export default defineConfig({
-  root: resolve(__dirname),
   build: {
     outDir: "./docs",
     rolldownOptions: {
@@ -13,9 +13,29 @@ export default defineConfig({
       },
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern",
+        importers: [new NodePackageImporter()],
+      }
+    }
+  },
   plugins: [basicSsl()],
+  resolve: {
+    alias: {
+      "@app": resolve(__dirname, "src/app"),
+      "@lib": resolve(__dirname, "src/lib"),
+      "@lib/classes": resolve(__dirname, "src/lib/classes"),
+      "@lib/extensions": resolve(__dirname, "src/lib/extensions"),
+      "@lib/modules": resolve(__dirname, "src/lib/modules"),
+      "@lib/types": resolve(__dirname, "src/lib/types"),
+      "@lib/utils": resolve(__dirname, "src/lib/utils"),
+    },
+  },
+  root: resolve(__dirname),
   server: {
-    host: 'localhost',
+    host: "localhost",
     https: true,
     port: 5173,
   },
